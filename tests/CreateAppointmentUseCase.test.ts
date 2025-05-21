a4f01008426298261e683608b36b4dbcfe66fa48
Eduardo Cabrera Japa <dvex.cabrera@gmail.com>
2025-05-21 00:47:59 -0500

    Create unit test

---
+++ tests/CreateAppointmentUseCase.test.ts
@@ -0,0 +1,29 @@
+import { CreateAppointment } from '../src/application/usecases/CreateAppointment'
+import { Appointment } from '../src/domain/entities/Appointment'
+
+describe('CreateAppointmentUseCase', () => {
+  const mockRepo = {
+    save: jest.fn()
+  }
+
+  const mockPublisher = {
+    publishCreated: jest.fn()
+  }
+
+  it('should create and save an appointment, and publish event', async () => {
+    const useCase = new CreateAppointment(mockRepo, mockPublisher)
+
+    const input = { insuredId: '12345', scheduleId: 100, countryISO: 'PE' }
+
+    const result = await useCase.execute(input)
+
+    expect(result).toBeInstanceOf(Appointment)
+    expect(result.insuredId).toBe('12345')
+    expect(result.scheduleId).toBe(100)
+    expect(result.countryISO).toBe('PE')
+    expect(result.status).toBe('pending')
+
+    expect(mockRepo.save).toHaveBeenCalledWith(expect.any(Appointment))
+    expect(mockPublisher.publishCreated).toHaveBeenCalledWith(expect.any(Appointment))
+  })
+})
